c824f7900a4b6b99ab67ad474261f82c3cbe03dc
otiteakpofure <akpofure.otite@gmail.com>
2022-07-18 19:06:49 +0100

    accordion project for frequently asked questions

---
+++ accordian/accordian.js
@@ -0,0 +1,10 @@
+// varibale declaration
+
+const accordian = document.getElementsByClassName("content-container");
+
+
+for( i=0; i < accordian.length; i++){
+    accordian[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+    })
+}
